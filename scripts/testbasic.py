ccf87e2e2f194324f783141f30ef1de52f232954
miaosi <miaosi@192.168.0.101>
2022-04-04 01:57:41 +0800

    fix

--- scripts/testbasic.py
+++ scripts/testbasic.py
@@ -3,7 +3,7 @@ from collision import Rect, Vars
 import matplotlib.pyplot as plt
 
 v = Vars()
-r = Rect(10, 2, 30*v.iarc, 5, 2)
+r = Rect(10, 2, 5, 2, 30*v.iarc)
 print(r)
 coords = r.get()
 coords.append(coords[0]) #Get a closed loop!
