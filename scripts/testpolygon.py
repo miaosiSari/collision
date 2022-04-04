3e177fbaf042124785f658fa4c466693e79b5a9f
miaosi <miaosi@192.168.0.101>
2022-04-04 15:37:39 +0800

    Complicated cases

--- scripts/testpolygon.py
+++ scripts/testpolygon.py
@@ -2,6 +2,7 @@ from matplotlib.patches import Polygon
 from collision import *
 from math import *
 import matplotlib.pyplot as plt
+import time
 
 want_to_collide = False
 v = Vars()
@@ -18,10 +19,15 @@ for i in range(5): #画一个5边形
     start[1] += edge[1]
     tmp1, tmp2 = edge[0] * cos(round) - edge[1] * sin(round), edge[0] * sin(round) + edge[1] * cos(round)
     edge = [tmp1, tmp2]
-p5 = Polygon(vertices) 
-vertices.append(vertices[0])
-p6 = Polygon([(1.0, 0.0), (cos(delta), sin(delta)), (-cos(delta), sin(delta)),\
-        (-1.0, 0.0), (-cos(delta), -sin(delta)), (cos(delta), -sin(delta))])
+
+starttime = time.time()
+for i in range(1000):
+    p5 = Polygon(vertices) 
+    vertices.append(vertices[0])
+    p6 = Polygon([(1.0, 0.0), (cos(delta), sin(delta)), (-cos(delta), sin(delta)),\
+            (-1.0, 0.0), (-cos(delta), -sin(delta)), (cos(delta), -sin(delta))])
+print(time.time() - starttime)
+
 coords = p6.get()
 coords.append(coords[0])
 xs, ys = zip(*vertices)
